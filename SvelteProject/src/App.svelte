b8285328ade9db5bdae55ccb69be42c723c9f189
Hiro <pepapek@gmail.com>
2021-10-16 12:09:19 +0200

    Strona główna

--- SvelteProject/src/App.svelte
+++ SvelteProject/src/App.svelte
@@ -1,30 +1,74 @@
 <script>
-	export let name;
+	 import Nav from "./Components/Nav.svelte";
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
+<Nav/>
+
+<section class="pannels">
+<div class="ratol">
+	<h1>Panel Ratownika</h1>
+</div>
+<div class="pacjent">
+	<h1>Panel Poszkodowanego</h1>
+</div>
+</section>
+
+
+
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
+
 
 	h1 {
-		color: #ff3e00;
+		color: black;
+		text-shadow: 2px 2px #ccc;
 		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
+		font-size: 2.5em;
+		font-weight: 500;
+		margin: 0;
+		display: block;
+		/* background-color: rgba(150,150,150, .4); */
+		width: 100%;
+		line-height: 150%;
+		/* padding: 10px; */
 
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
 	}
+	section{
+		width: 100%;
+		height: 100vh;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+
+
+	}
+	div{
+		/* padding-top: 160px; */
+		cursor: pointer;
+		padding-bottom: 20px;
+		width: 42vh;
+		height: 42vh;
+		text-align: center;
+		border: 5px solid black;
+		margin: 40px;
+		/* z-index: auto; */
+		border-radius: 30px;
+		overflow: hidden;
+		box-shadow: 5px 5px 10px black ;
+		display: flex;
+		flex-direction: column;
+		justify-content: flex-end;
+	}
+	div.ratol{
+
+		background-image: url('https://cdn.pixabay.com/photo/2016/09/16/19/12/ambulance-1674877_960_720.png');
+		background-position: center;
+		background-size: cover;
+	}
+	div.pacjent{
+		background-image: url('https://cdn.pixabay.com/photo/2016/09/16/19/15/drug-1674890_960_720.png');
+		background-position: center;
+		background-size: cover;
+	}
+	
 </style>
---
+++ SvelteProject/src/Components/Nav.svelte
@@ -0,0 +1,30 @@
+<script>
+export let isLogged = true;
+import BurgerMenu from 'svelte-burger-menu';
+</script>
+
+   
+    <BurgerMenu menuColor = {'#000'} padding = {'25px'} backgroundColor = {'#548B95'} >
+        <li>Tutorial</li>
+        <li>Mapa Aptek</li>
+        <li>Oceń aplikację</li>
+        {#if isLogged}
+        <li>Wyloguj</li>
+        {:else}
+        <li>Zaloguj</li>
+        {/if}
+    </BurgerMenu>
+
+    <!-- <i class="fas fa-align-left"></i> -->
+    
+
+<style>
+
+    li{
+        font-size: 30px;
+        margin: 10px;
+        text-transform: uppercase;
+        letter-spacing: 1px;
+        list-style: none;
+    }
+</style>
